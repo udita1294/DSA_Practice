520cde7ed352555debb37c65e854956820aeda3c
UDITA SINGH <udita1294@gmail.com>
2026-07-07 23:59:15 +0530

    Time: 44 ms (75.36%), Space: 29 MB (92.64%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        int n = nums.size();
+
+        for (int i = 0; i < n - 2; i++) {
+
+            // Skip duplicate values for i
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == 0) {
+
+                    ans.push_back({nums[i], nums[j], nums[k]});
+
+                    j++;
+                    k--;
+
+                    // Skip duplicates for j
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+
+                    // Skip duplicates for k
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
+                }
+                else if (sum < 0) {
+                    j++;
+                }
+                else {
+                    k--;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
