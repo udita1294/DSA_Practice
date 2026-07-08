141649ebf5d3ee141bbabae770d94abdbaae54b4
UDITA SINGH <udita1294@gmail.com>
2026-07-08 14:19:15 +0530

    Time: 0 ms (100%), Space: 31.4 MB (46.13%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int s = 0;
+        int e =  n-1;
+        int mid;
+
+        while(s <= e){
+            mid = s + (e-s)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                s =  mid + 1;
+            }else{
+                e = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
