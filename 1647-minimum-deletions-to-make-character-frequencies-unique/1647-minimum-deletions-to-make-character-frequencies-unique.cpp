e796e21d273bd53191259ba74b51d040b68ab94f
UDITA SINGH <udita1294@gmail.com>
2026-08-01 01:23:41 +0530

    Time: 0 ms (100%), Space: 20.4 MB (48.08%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        int freq[26] = {0};
+        for(char &ch : s){
+            freq[ch - 'a']++;
+        }
+        int ans = 0;
+        unordered_set<int>st;
+        
+        for(int i=0;i<26;i++){
+            while(freq[i] > 0 && st.find(freq[i]) != st.end()){
+                freq[i] -= 1;
+                ans++;
+            }
+            st.insert(freq[i]);
+        }
+        return ans;
+    }
+};
