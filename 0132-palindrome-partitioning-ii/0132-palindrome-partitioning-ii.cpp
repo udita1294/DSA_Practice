c627d053aceb903df8f0e3e4bb95a41aa89958af
UDITA SINGH <udita1294@gmail.com>
2026-08-10 02:30:02 +0530

    Time: 1669 ms (22.25%), Space: 26.5 MB (22.2%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,53 +1,30 @@
 class Solution {
 public:
     int t[2001][2001];
-
-    bool isPalindrome(string &s, int i, int j) {
-        while(i < j) {
-            if(s[i] != s[j])
-                return false;
-
+    bool isPalindrome(string &s,int i,int j){
+        while(i < j){
+            if(s[i] != s[j]) return false;
             i++;
             j--;
         }
-
         return true;
     }
-
-    int solve(string &s, int i, int j) {
-
-        // No cut needed
-        if(i >= j)
-            return 0;
-
-        // If entire substring is palindrome
-        if(isPalindrome(s, i, j))
-            return 0;
-
-        if(t[i][j] != -1)
-            return t[i][j];
-
+    int solve(string &s,int i,int j){
+        if(i >= j) return 0;
+        if(isPalindrome(s,i,j))return 0;
+        if(t[i][j] != -1) return t[i][j];
         int mini = INT_MAX;
-
-        for(int k = i; k < j; k++) {
-
-            // Make a cut only if s[i...k] is palindrome
-            if(isPalindrome(s, i, k)) {
-
-                int temp = solve(s, k + 1, j) + 1;
-
-                mini = min(mini, temp);
+        for(int k=i;k<j;k++){
+            if(isPalindrome(s,i,k)){
+                int temp = 1 + solve(s,k+1,j);
+                mini = min(mini,temp);
             }
         }
-
         return t[i][j] = mini;
     }
-
     int minCut(string s) {
         int n = s.size();
-
-        memset(t, -1, sizeof(t));
-
-        return solve(s, 0, n - 1);
+        memset(t,-1,sizeof(t));
+        return solve(s,0,n-1);
     }
 };
