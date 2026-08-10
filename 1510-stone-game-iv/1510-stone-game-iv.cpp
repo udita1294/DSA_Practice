0cbe4f52da1ece6c46ccd710511083968552fdfd
UDITA SINGH <udita1294@gmail.com>
2026-08-10 15:21:35 +0530

    Time: 35 ms (71.21%), Space: 13.9 MB (58.98%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int dp[100001];
+    int solve(int n){
+        if(n <= 0) return 0;
+        if(dp[n] != -1) return dp[n];
+        for(int i=1; i*i <=n;i++){
+            if(solve(n - i*i) == 0) return dp[n] = 1;
+        }
+        return dp[n] = 0;
+    }
+    bool winnerSquareGame(int n) {
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
+    }
+};
