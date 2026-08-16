6a691fcf6f744a5c59bab6265189507284e78c2d
UDITA SINGH <udita1294@gmail.com>
2026-08-16 15:37:56 +0530

    Time: 0 ms (100%), Space: 92.6 MB (44.91%) - LeetHub

---
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        int n = arr.size();
+        vector<int> prefix(n);
+        prefix[0] = arr[0];
+
+        for(int i = 1; i < n; i++){
+            prefix[i] = prefix[i - 1] ^ arr[i];
+        }
+        vector<int> answer;
+        for(int i = 0; i < queries.size(); i++){
+            int l = queries[i][0];
+            int r = queries[i][1];
+            if(l == 0){
+                answer.push_back(prefix[r]);
+            }
+            else{
+                answer.push_back(prefix[r] ^ prefix[l - 1]);
+            }
+        }
+        return answer;
+    }
+};
